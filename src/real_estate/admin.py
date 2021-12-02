ff55d4b1ef3891bf8b62f6bb28e39db7512d0ca3
Akbarbek Umurzakov <thisisumurzakov@gmail.com>
2021-12-02 19:51:29 +0500

    admin

--- src/real_estate/admin.py
+++ src/real_estate/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from .models import Flat
+from .models import Flat, Office, Sector, Vacation_home, Other_r
 
 admin.site.register(Flat)
+admin.site.register(Office)
+admin.site.register(Sector)
+admin.site.register(Vacation_home)
+admin.site.register(Other_r)
--- src/transport/admin.py
+++ src/transport/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from .models import Car, Brand, Model, Color, BodyType
+from .models import Car, Brand, Model, Color, BodyType, Truck, Agricultural, Special, Other_t
 
 admin.site.register(Car)
 admin.site.register(Brand)
 admin.site.register(Model)
 admin.site.register(Color)
 admin.site.register(BodyType)
+admin.site.register(Truck)
+admin.site.register(Agricultural)
+admin.site.register(Special)
+admin.site.register(Other_t)
